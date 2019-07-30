7c26c6ee8981c5e79ddd76b3afd4ded3a6accce2
Jocy15 <jociben15@gmail.com>
2019-07-30 13:01:48 -0500

    Initial Commit

---
+++ CustomLoginDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomLoginDemo
+//
+//  Created by Central States SER 02 on 7/30/19.
+//  Copyright © 2019 Central States SER 02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
